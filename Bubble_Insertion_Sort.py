93285a3e97ab240658c78a35a7d4b98f5980d81e
Jessica <Jessica@DESKTOP-U34H195>
2026-02-05 09:15:19 +1300

    Bubble Sort Added

---
+++ Bubble_Insertion_Sort.py
@@ -0,0 +1,9 @@
+mylist = [67, 12, 89, 43, 56, 34, 78, 23, 91, 45, 18, 76, 39, 52, 87, 65, 29, 83, 16, 72, 47, 54, 31, 95, 68, 21, 84, 59, 13, 75]
+
+n = len(mylist)
+for i in range(n-1):
+  for j in range(n-i-1):
+    if mylist[j] > mylist[j+1]:
+      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+
+print(mylist)
