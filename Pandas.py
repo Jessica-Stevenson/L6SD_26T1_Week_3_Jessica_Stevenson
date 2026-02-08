b0d6074a499e047051f0537e7a1ee20870cf90f9
Jessica <Jessica@DESKTOP-U34H195>
2026-02-09 09:24:03 +1300

    baisc panda algorithm

--- Pandas.py
+++ Pandas.py
@@ -1,2 +1,10 @@
 import pandas as pd
 
+import numpy as np
+
+s = pd.Series() 
+print("Pandas Series: ", s) 
+data = np.array(['C', 'l', 'a', 'i', 'r', '-', 'O', 'b', 's', 'c', 'u', 'r']) 
+  
+s = pd.Series(data) 
+print("Pandas Series:\n", s)
